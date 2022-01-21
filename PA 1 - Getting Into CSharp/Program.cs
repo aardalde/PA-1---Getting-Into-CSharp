5f9afeabf302c0ec0613fdd599db795ab448976d
Aaron Alden <aardalde@ut.utm.edu>
2022-01-21 10:44:35 -0600

    Add project files.

---
+++ PA 1 - Getting Into CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA_1___Getting_Into_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
